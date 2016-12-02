ffd7cca83408b40545f0cf4e034d5d979979de71
patjm1992 <patjm1992@gmail.com>
2016-12-01 22:43:37 -0500

    added .py source files

---
+++ markov.py
@@ -0,0 +1,161 @@
+import random
+import json
+import argparse
+import timeit
+
+'''
+	Description of file goes here
+'''
+
+def generate_text(markov_chain, desired_len):
+	'''
+		Requires the markov chain dictionary/table
+		Returns a string of new text
+	'''
+	key = random.sample(list(markov_chain), 1)
+	word_1, word_2 = ("".join(key[0])).split()
+
+	new_passage = ''
+
+	for i in range(desired_len):
+		try:
+			new_word = random.choice(markov_chain[" ".join(key)])
+		except KeyError:
+			print("Key not found: " + word_1, word_2)
+			break
+
+		new_passage += new_word + ' '
+		word_1 = word_2
+		word_2 = new_word
+		combo = word_1 + ' ' + word_2
+		key = combo.split()
+
+	return new_passage
+
+def ends_with_punctuation(w):
+	'''
+		Return True if word ends in a period, a question mark, or an exclamation
+		False otherwise
+	'''
+	punct = ['.', '?', '!']
+
+	return w[-1:] in punct
+
+def has_quote(w):
+	'''
+		Return True if a word has a quote as the first or last character
+		False otherwise
+	'''
+
+	return w[-1:] is "'" or w[:1] is "'"
+
+def add_capitalization(passage):
+	'''
+		Requires a string of text
+		Returns a string of text that is (mostly) properly capitalized
+	'''
+
+	word_list = passage.split()
+
+	for i, word in enumerate(word_list):
+		if ends_with_punctuation(word):
+			try:
+				word_list[i + 1] = word_list[i + 1].capitalize()
+			except IndexError:
+				break
+
+	# Capitalize the first letter in the newly generated text
+	word_list[0] = word_list[0].capitalize()
+
+	capitalized = " ".join(word_list)
+
+	return capitalized
+
+def strip_quotes(passage):
+	'''
+		Requires a string of text
+		Returns a string where instances like 'hello or hello' are removed
+	'''
+	word_list = passage.split()
+
+	for i, word in enumerate(word_list):
+		if has_quote(word_list[i]):
+			word_list.remove(word)
+			a = list(word)
+			a.remove("'")
+			word_list[i] = "".join(a)
+
+	without_quotes = " ".join(word_list)
+
+	return without_quotes
+
+def trim_by_chars(passage, num_chars):
+	'''
+		Requires a string of text
+		Returns a string of specified length
+	'''
+
+
+	num_chars = num_chars - 3
+
+	if passage[(num_chars - 1):num_chars] == ' ':
+		passage = passage[:num_chars]
+		passage += '...'
+	elif passage[(num_chars - 1):num_chars] == '.':
+		passage = passage[:num_chars]
+	else:
+		passage = passage[:num_chars]
+		passage += '-'
+
+	return passage
+
+def add_ending_punctuation(passage):
+
+	sentence_ender = ['.', '...', '!', '?']
+
+	word_list = passage.split()
+
+	if ends_with_punctuation(word_list[len(word_list) - 1]):
+		if ',' in word_list[len(word_list) - 1]:
+			word_list[len(word_list) - 1] = word_list[len(word_list) - 1][:-1]
+			return " ".join(word_list)
+		else:
+			return passage
+	else:
+		return passage + random.choice(sentence_ender)
+
+def format(text):
+	'''
+	 	Wrapper function
+	'''
+	return add_ending_punctuation(strip_quotes(add_capitalization(text)))
+
+def handle_cmd_args():
+	descript_msg = 'Use a JSON file (representing a Python dict that in turn' \
+				  'represents a markov chain) to generate the text from the' \
+				  'markov chain.'
+	help_msg = 'Desired length of your generated text (in words).'
+	parser = argparse.ArgumentParser(description=descript_msg)
+	parser.add_argument('-f', '--json-file', required=True)
+	parser.add_argument('-wc', '--word-count', help=help_msg, required=True)
+	return parser.parse_args()
+
+def main():
+	args = handle_cmd_args()
+
+	start_time = timeit.default_timer()
+
+	with open(args.json_file) as fp:
+		markov_chain = json.load(fp)
+
+	text = generate_text(markov_chain, int(args.word_count))
+
+
+
+	print(format(text))
+
+
+
+if __name__ == '__main__':
+	main()
+
---
+++ to_json.py
@@ -0,0 +1,77 @@
+import json
+import argparse
+
+'''
+	Description of file goes here
+'''
+
+def file_to_text(file_name):
+	'''
+		Requires a filename
+		Returns a list where each element is a single word, and
+		where all standalone digits have been stripped from the text (chapter
+		numbers and the like)
+	'''
+	f = open(file_name)
+	words = []
+
+	# Read .txt file into gigantic list of words
+	for word in f.read().split():
+		words.append(word)
+
+	return [x for x in words if not x.isdigit()]
+
+def create_markov(source_text):
+	'''
+		Requires a source text
+		Returns the markov chain dictionary/table
+	'''
+	markov_chain = {}
+
+	# start with first two words in the text
+	word_1 = source_text[0]
+	word_2 = source_text[1]
+
+	for i in range(len(source_text) - 1):
+		try:
+			word_1 = source_text[i]
+			word_2 = source_text[i + 1]
+			word_3 = source_text[i + 2]
+		except IndexError:
+			break
+
+		word_pair = (word_1, word_2)
+		word_pair = ' '.join(word_pair)
+
+		if word_pair not in markov_chain:
+			markov_chain[word_pair] = []
+
+		markov_chain[word_pair].append(word_3)
+
+	return markov_chain
+
+def handle_cmd_args():
+	descrip_msg = 'Produce a JSON file holding a Python dictionary representing' \
+				  'markov chains built from a source text.'
+
+	help_msg = 'Your source text/corpus for building the markov chain(s) from.'
+	parser = argparse.ArgumentParser(description=descrip_msg)
+	parser.add_argument('-f', '--source-file', help=help_msg, required=True)
+	parser.add_argument('-o', '--output', help='Output filename for JSON file.', \
+						default="markov.json")
+
+	return parser.parse_args()
+
+def main():
+	args = handle_cmd_args()
+	text = file_to_text(args.source_file)
+	markov_dict = create_markov(text)
+
+	with open(args.output, 'w') as fp:
+		json.dump(markov_dict, fp, indent=4)
+
+
+if __name__ == '__main__':
+	main()
+
+
